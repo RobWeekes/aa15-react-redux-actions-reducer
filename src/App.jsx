538590147215abbcbea6e252ceaafdb4605d3479
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2025-02-06 17:08:54 -0800

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -4,4 +4,29 @@ function App() {
   return <h1> Hello from App </h1>;
 }
 
+// function App() {
+//   const [num, setNum] = useState(1);
+//   const [show, setShow] = useState(true);
+//   const message = `The number is ${num}`;
+
+//   useEffect(() => {
+//     console.log(num);
+//   });
+
+//   return (
+//     <>
+//       <h1> {message} </h1>
+
+//       <button onClick={() => setNum(prevNum => prevNum + 1)}>
+//         Add 1!
+//       </button>
+//       <button onClick={() => setShow(prevShow => !prevShow)}>
+//         {show ? "Hide" : "Show"} {num} Doubled
+//       </button>
+
+//       {show && <p> {num} doubled is {num*2} </p>}
+//     </>
+//   );
+// }
+
 export default App;
